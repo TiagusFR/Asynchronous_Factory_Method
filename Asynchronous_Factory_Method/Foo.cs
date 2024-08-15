87c9a5a36bb0f4807d3af0423815ad6c43301b5c
Tiago França <t.iago_196@hotmail.com>
2024-08-15 16:16:01 -0300

    Add project files.

---
+++ Asynchronous_Factory_Method/Foo.cs
@@ -0,0 +1,10 @@
+﻿namespace Asynchronous_Factory_Method
+{
+    public class Foo
+    {
+        public static async Task InitAsync()
+        {
+            await Task.Delay(1000);
+        }
+    }
+}
---
+++ Asynchronous_Factory_Method/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Asynchronous_Factory_Method
+{
+    internal class Program
+    {
+        public static async Task Main()
+        {
+            await Foo.InitAsync();
+        }
+    }
+}
